295e269ddeeb14544f3d9931253e260e5b6fb108
Steven Baum <stevenbaum@berkeley.edu>
2016-11-10 16:45:38 -0800

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,9 @@
+// Data for color-gradient slot & story performance
+var pageViewsSlotData = {};
 
+// Slot range to display on chart
+var slotRange = '1 4';
+
+var edition = 'English / United States';
+
+var platform = 'desktop';
